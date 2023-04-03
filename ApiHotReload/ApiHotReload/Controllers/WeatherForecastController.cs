2f6a3d40244c5690b123fac0ebe78fc97a6a853c
Júnior Veras <iramarjunhorveras@gmail.com>
2023-04-03 13:43:07 -0300

    17. Apresentando o recurso Hot Reload

---
+++ ApiHotReload/ApiHotReload/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiHotReload.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet("HotReloadTest")]
+        public IEnumerable<string> GetTesteHotReload()
+        {
+            return new string[] { "Teste Hot Reload 2", "Visual Studio 2022", ".NET 6" };
+        }
+
+    }
+}
