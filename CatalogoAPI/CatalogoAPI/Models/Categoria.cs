61f5f4b81c75f8cd66d65a5bd22c9583e50fd061
Júnior Veras <iramarjunhorveras@gmail.com>
2023-04-18 11:34:03 -0300

    62. Incluindo as dependências no projeto

---
+++ CatalogoAPI/CatalogoAPI/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace CatalogoAPI.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+
+        public ICollection<Produto>? Produtos{ get; set; }
+    }
+}
