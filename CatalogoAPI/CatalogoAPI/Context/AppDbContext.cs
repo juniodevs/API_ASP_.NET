4cb46129ae548f0b10469e530e5d53ea4c051583
Júnior Veras <iramarjunhorveras@gmail.com>
2023-04-18 11:54:30 -0300

    66. Fluent API - Configurando as propriedades e relacionamento

--- CatalogoAPI/CatalogoAPI/Context/AppDbContext.cs
+++ CatalogoAPI/CatalogoAPI/Context/AppDbContext.cs
@@ -12,7 +12,26 @@ namespace CatalogoAPI.Context
 
         protected override void OnModelCreating(ModelBuilder mb)
         {
-            
+            //Categoria
+            mb.Entity<Categoria>().HasKey(c => c.CategoriaId);
+            mb.Entity<Categoria>().Property(c => c.Nome).IsRequired().HasMaxLength(100);
+
+            mb.Entity<Categoria>().Property(c => c.Descricao)
+                                  .IsRequired()
+                                  .HasMaxLength(150);
+
+            //Produto
+            mb.Entity<Produto>().HasKey(p => p.ProdutoId);
+            mb.Entity<Produto>().Property(p => p.Nome).IsRequired().HasMaxLength(100);
+            mb.Entity<Produto>().Property(p => p.Descricao).IsRequired().HasMaxLength(150);
+            mb.Entity<Produto>().Property(p => p.Imagem).IsRequired().HasMaxLength(100);
+            mb.Entity<Produto>().Property(p => p.Preco).HasPrecision(14, 2);
+
+            //Relacionamento
+
+            mb.Entity<Produto>().HasOne(p => p.Categoria)
+                                .WithMany(c => c.Produtos)
+                                .HasForeignKey(p => p.CategoriaId);
         }
     }
 }
