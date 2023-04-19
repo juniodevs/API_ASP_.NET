4741fd7ca1a7243c3509498552996e93af7c1a7a
Júnior Veras <iramarjunhorveras@gmail.com>
2023-04-19 14:25:02 -0300

    74. Criando os endpoints para Produto

--- CatalogoAPI/CatalogoAPI/Models/Produto.cs
+++ CatalogoAPI/CatalogoAPI/Models/Produto.cs
@@ -1,4 +1,6 @@
-﻿namespace CatalogoAPI.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CatalogoAPI.Models
 {
     public class Produto
     {
@@ -10,6 +12,7 @@
         public DateTime DataCompra { get; set; }
         public int? Estoque { get; set; }
         public int CategoriaId { get; set; }
+        [JsonIgnore]
         public Categoria? Categoria { get; set; }
     }
 }
