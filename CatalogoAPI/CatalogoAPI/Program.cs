87530f908bfff426f881da312ead2ec695f7fffd
Júnior Veras <iramarjunhorveras@gmail.com>
2023-04-20 13:33:22 -0300

    76. Autenticação JWT - Implementação

--- CatalogoAPI/CatalogoAPI/Program.cs
+++ CatalogoAPI/CatalogoAPI/Program.cs
@@ -1,6 +1,11 @@
 using CatalogoAPI.Context;
 using CatalogoAPI.Models;
+using CatalogoAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,10 +19,54 @@ builder.Services.AddDbContext<AppDbContext>(options =>
                                             .UseMySql(connectionString,
                                             ServerVersion.AutoDetect(connectionString)));
 
+builder.Services.AddSingleton<ITokenServices>(new TokenService());
+
+builder.Services.AddAuthentication
+        (JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+               options.TokenValidationParameters = new TokenValidationParameters
+        {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+        (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            };
+        });
+
 var app = builder.Build();
 
+// endpoint para LOGIN
+
+app.MapPost("/login", [AllowAnonymous] (UserModel userModel, ITokenServices tokenService) =>
+{
+    if (userModel == null)
+    {
+        return Results.BadRequest("Login Inválido");
+    }
+    if(userModel.Username == "junio" && (userModel.Password == "123@123"))
+    {
+            var tokenString = tokenService.GerarToken(app.Configuration["Jwt:Key"],
+                                    app.Configuration["Jwt:Issuer"],
+                                    app.Configuration["Jwt:Audience"],
+                                    userModel);
+
+        return Results.Ok(new { token = tokenString });
+    }
+    else
+        return Results.BadRequest("Login Inválido");
+}).Produces(StatusCodes.Status400BadRequest)
+.Produces(StatusCodes.Status200OK)
+.WithName("Login")
+.WithTags("Authenticacao");
+
 //definir endpoints para CATEGORIAS
-app.MapGet("/", () => "Catálogo de Produtos - 2023").ExcludeFromDescription();
+app.MapGet("/", () => "Catálogo de Produtos - 2023").ExcludeFromDescription().RequireAuthorization();
 
 app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
 {
@@ -82,7 +131,7 @@ app.MapPost("/produtos", async(AppDbContext db, Produto produto) =>
     return Results.Created($"/produtos/{produto.CategoriaId}", produto);
 });
 
-app.MapGet("/produtos", async(AppDbContext db) => await db.Produtos.ToListAsync());
+app.MapGet("/produtos", async(AppDbContext db) => await db.Produtos.ToListAsync()).RequireAuthorization();
 
 app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
 {
@@ -132,4 +181,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
 app.Run();
---
+++ CatalogoAPI/CatalogoAPI/Services/ITokenServices.cs
@@ -0,0 +1,7 @@
+﻿using CatalogoAPI.Models;
+
+namespace CatalogoAPI.Services;
+public interface ITokenServices
+{
+    string GerarToken(string key, string issuer, string audience, UserModel user);
+}
