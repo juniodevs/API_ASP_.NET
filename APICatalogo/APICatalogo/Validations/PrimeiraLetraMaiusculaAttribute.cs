8225aae2f46850603c74ea7ccbe645daa6857249
Júnior Veras <iramarjunhorveras@gmail.com>
2023-05-04 12:34:13 -0300

    91. Validação

--- APICatalogo/APICatalogo/Validations/PrimeiraLetraMaiusculaAttribute.cs
+++ APICatalogo/APICatalogo/Validations/PrimeiraLetraMaiusculaAttribute.cs
@@ -5,15 +5,14 @@ namespace ApiCatalogo.Validations
     public class PrimeiraLetraMaiusculaAttribute : ValidationAttribute
     {
         protected override ValidationResult IsValid(object value,
-            ValidationContext validationContext)
+            ValidationContext validationContext) // ValidationContext é o contexto de validação
         {
-            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            if (value == null || string.IsNullOrEmpty(value.ToString())) // se o valor for nulo ou vazio, retorna sucesso
             {
                 return ValidationResult.Success;
             }
-
-            var primeiraLetra = value.ToString()[0].ToString();
-            if (primeiraLetra != primeiraLetra.ToUpper())
+            var primeiraLetra = value.ToString()[0].ToString(); // pega a primeira letra do valor
+            if (primeiraLetra != primeiraLetra.ToUpper()) // se a primeira letra não for maiúscula, retorna erro
             {
                 return new ValidationResult("A primeira letra do nome do produto deve ser maiúscula");
             }
